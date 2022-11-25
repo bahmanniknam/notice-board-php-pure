2b2bf79c32376b7fc86ab132513265fd1ce07f1d
Bahman <nightbn1990@gmail.com>
2022-11-26 02:38:02 +0330

    create validation

--- public/create.php
+++ public/create.php
@@ -1,15 +1,21 @@
 <?php
+
 ob_start();
 
 require "../vendor/autoload.php";
 
 use Bahman\NoticeBoard\Repositories\Notice;
 
-$notice = new Notice();
-$notice->title = $_POST['title'];
-$notice->content = $_POST['content'];
-$notice->user_id = $_POST['user_id'];
-$notice->create();
+if (empty($_POST["title"]) || empty($_POST["content"]) ||empty($_POST["user_id"]) ) {
+    $errMsg = "Error! All fields is required.";
+    header("Location: errors/404.php?message=" . $errMsg);
+} else {
+    $notice = new Notice();
+    $notice->title = $_POST['title'];
+    $notice->content = $_POST['content'];
+    $notice->user_id = $_POST['user_id'];
+    $notice->create();
 
-header("Location: index.php");
+    header("Location: index.php");
+}
 
--- public/errors/404.php
+++ public/errors/404.php
@@ -1,3 +1,12 @@
-<?php
 
-echo $_GET['message'];
+<?php include '../partials/header.php';?>
+
+<?php include '../partials/navbar.php';?>
+<div>
+    <div class="d-flex align-items-center justify-content-center vh-100 bg-primary">
+        <h7 class="display-1 fw-bold text-white"><?php echo $_GET['message']; ?></h7>
+    </div>
+</div>
+<?php include '../partials/footer.php';?>
+
+
